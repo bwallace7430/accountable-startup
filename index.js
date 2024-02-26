e9623de76db79cb19837f6b2fb6cd7358ac8dd3f
Brynne Henderson <bwallace7430@gmail.com>
2024-02-25 18:36:26 -0700

    login function

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-function createUser() {
-    email = document.getElementById("emailInput");
-    password = document.getElementById("passwordInput");
-    all_users = localStorage.getItem("Users")
-    let user = { email: email, password: password }
+function login() {
+    const email = document.getElementById('emailInput').value;
+    localStorage.setItem("email", email);
+    window.location.href = "journal.html";
 }
