c396293363f4adb59836d487b9714f977fe4cbbc
Brynne Henderson <bwallace7430@gmail.com>
2024-02-27 20:07:07 -0700

    create friend and active writers function

---
+++ friends.js
@@ -0,0 +1,38 @@
+addEventListener("load", setupPage);
+
+function setupPage() {
+    if (localStorage.getItem("friendsList") === null) {
+        localStorage.setItem("friendsList", JSON.stringify(["Imhotep", "Lord Scotland", "Abraham Lincoln", "Rhianna", "Oliver", "Esme", "My Favorite TA"]));
+    }
+    displayList();
+}
+
+function displayList() {
+    let friendsList = JSON.parse(localStorage.getItem("friendsList"));
+    for (let friend of friendsList) {
+        addFriendToDisplay(friend)
+    }
+}
+
+function addFriendToDisplay(friend) {
+    let li = document.querySelector('#friends').appendChild(document.createElement("li"));
+    let span = li.appendChild(document.createElement("span"));
+    span.className = "friend_activity_indicator inactive";
+    let p = li.appendChild(document.createElement('p'));
+    p.innerHTML = friend;
+}
+
+function createFriend() {
+    let friendsList = JSON.parse(localStorage.getItem("friendsList"));
+    let newFriend = document.getElementById('username_input').value;
+    friendsList.push(newFriend);
+    localStorage.setItem("friendsList", JSON.stringify(friendsList));
+    addFriendToDisplay(newFriend);
+}
+
+setInterval(() => {
+    let inactiveFriends = document.querySelectorAll('.inactive');
+    if (inactiveFriends.length === 0) { return; }
+    let randomInactive = inactiveFriends.item(Math.random() * inactiveFriends.length);
+    randomInactive.className = "friend_activity_indicator active";
+}, 5000);
