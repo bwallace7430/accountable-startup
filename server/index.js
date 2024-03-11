2cb3d60ea80bd79632fa3d04416d3413d0dd125f
Brynne Henderson <bwallace7430@gmail.com>
2024-03-10 21:24:27 -0600

    name endpoints

--- server/index.js
+++ server/index.js
@@ -4,7 +4,22 @@ const PORT = 8080;
 
 server.use(express.static('public'));
 
+server.post("/users")
+server.post("/sessions")
+server.post("/users/:userid/entries")
+server.get("/users/:userid/entries")
+server.get("/users/:userid/friends")
+server.post("/users/:userid/friends")
+
 // Start the server
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+//ENDPOINTS:
+// POST createUSER
+// POST log in user
+// POST create journal entry
+// GET read a journal entry
+// GET list of friends
+// POST add a friend
