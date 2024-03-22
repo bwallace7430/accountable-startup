a83666f48304e492e01d017b617e4506547b90c8
Brynne Henderson <bwallace7430@gmail.com>
2024-03-21 18:14:58 -0600

    friends, entries, and logout data

--- server/data.js
+++ server/data.js
@@ -1,10 +1,5 @@
-const users = [];
-const entries = {};
-const friends = {};
-
 const userCollection = client.db('authTest').collection('user');
 const entryCollection = client.db('authTest').collection('entry');
-const friendCollection = client.db('authTest').collection('friend');
 
 export async function getUser() {
     return userCollection.findOne({ username: username });
@@ -17,65 +12,62 @@ export async function createUser(username, password) {
         const user = {
             username: username,
             password: passwordHash,
-            token: generateAuthToken()
+            token: generateAuthToken(),
+            friends: []
         };
         await userCollection.insertOne(user);
     }
     else {
-        throw new Error("Username already taken.")
+        throw new Error("Username already taken.");
     }
 }
 
 export async function createSession(username, password) {
 
-    let user = await getUser(username)
+    let user = await getUser(username);
     if (!user) {
-        throw new Error("User not found")
+        throw new Error("User not found");
     }
     let passwordHash = await bcrypt.hash(password, 10);
     if (!user.password === passwordHash) {
-        throw new Error("Password is incorrect")
+        throw new Error("Password is incorrect");
     }
 
     let authToken = generateAuthToken();
-    userCollection.updateOne({ username: username }, { $set: { token: authToken } })
-    user = await getUser(username)
-    return user.authToken;
+    userCollection.updateOne({ username: username }, { $set: { token: authToken } });
+
+    return authToken;
 }
 
 function generateAuthToken() {
-    return uuid.v4()
+    return uuid.v4();
 }
 
-export function createEntry(userId, day, entry) {
-    if (!entries[userId]) {
-        entries[userId] = {}
-    }
-    let allEntries = entries[userId]
-    allEntries[day] = entry;
+export async function createEntry(username, day, entry) {
+    const newEntry = {
+        username: username,
+        date: day,
+        entry: entry
+    };
+    return await entryCollection.insertOne(newEntry);
 }
 
-export function getUserEntry(userId, day) {
-    if (!entries[userId]) {
-        throw new Error("User has no entries")
-    }
-    if (!entries[userId][day]) {
-        throw new Error("Day has no entry")
-    }
-    return entries[userId][day]
+export async function getUserEntry(username, day) {
+    return await entryCollection.findOne({ username: username, date: day });
 }
 
-export function addFriend(userId, friendUsername) {
-    if (!friends[userId]) {
-        friends[userId] = []
+export async function addFriend(username, friendUsername) {
+    if (!await getUser(friendUsername)) {
+        throw new Error("This user does not exist.");
     }
-    let allFriends = friends[userId]
-    allFriends.push(friendUsername)
+    return await userCollection.updateOne({ username: username }, { $push: { friends: friendUsername } });
 }
 
-export function getFriends(userId) {
-    if (!friends[userId]) {
-        throw new Error("User has no friends")
-    }
-    return friends[userId]
+export async function getFriends(username) {
+    let user = await getUser(username);
+    return user.friends;
+}
+
+export async function deleteSession(username) {
+    return await userCollection.updateOne({ username: username }, { $set: { token: null } });
 }
